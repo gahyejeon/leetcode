28d0fcceeb9c650161f2643ec293bfabc8a0914b
Hye <gahyejeon@gmail.com>
2025-03-25 14:11:23 +0900

    Time: 2 ms (14.29%), Space: 20.1 MB (8.73%) - LeetHub

---
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func findNumbers(_ nums: [Int]) -> Int {
+        var count = 0
+        
+        for num in nums {
+            if String(num).count % 2 == 0 {
+                count += 1
+            }
+        }
+        return count
+    }
+}
+
+
+// 자릿수 갯수 카운트 
