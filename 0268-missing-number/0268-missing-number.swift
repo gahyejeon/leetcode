1f6a26d5660c288682b94b1979e8eb2e3a57c1f6
Hye <gahyejeon@gmail.com>
2025-05-21 12:12:55 +0900

    Time: 0 ms (100%), Space: 19.9 MB (29.53%) - LeetHub

--- 0268-missing-number/0268-missing-number.swift
+++ 0268-missing-number/0268-missing-number.swift
@@ -1,17 +1,31 @@
 class Solution {
     func missingNumber(_ nums: [Int]) -> Int {
-        let sorted = nums.sorted()
+        var nums = nums
+        var i = 0
 
-        for i in 0..<sorted.count {
-            if sorted[i] != i {
+        while i < nums.count {
+            let val = nums[i]
+            if val < nums.count && nums[i] != nums[val] {  // val 인덱스 범위 안에서 제자리에 없으면 swap
+                nums.swapAt(i, val)   // swapAt 인덱스 위치 바꿔주는 함수
+            } else {
+                i += 1
+            }
+        }
+
+
+        // 값이 인덱스랑 다르면 그 인덱스가 빠진 숫자가 됨
+        for i in 0..<nums.count {  
+            if nums[i] != i {
                 return i
             }
         }
+
+        // 다 같으면 빠진 숫자는 n이 됨
         return nums.count
     }
 }
 
 
-// 정렬하고
-// 인덱스 넘버랑 자리에 있는 수랑 같은지 확인
-// 다른 부분이 빠진 숫자임
+
+
+
